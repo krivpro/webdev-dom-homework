38d1e1c4bb87ddc5c3942fd9463d18b2949f001e
krivpro <krivservice@gmail.com>
2025-07-07 01:51:43 +0300

    сделан текст при прогрузке

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { updateComments } from './modules/comments.js'
 import { initAddListener } from './modules/initListeners.js'
 import { renderComments } from './modules/renderComments.js'
 
+document.querySelector(".comments").innerHTML = 'Пожалуйста дождитесь загрузки комментариев...'
+
 fetchComments().then(data => {
     updateComments(data)
     renderComments()
--- modules/initListeners.js
+++ modules/initListeners.js
@@ -45,8 +45,14 @@ export const initAddListener = (renderComments) => {
             return
         }
 
+        document.querySelector('.form-loading').style.display = 'block'
+        document.querySelector('.add-form').style.display = 'none'
 
         postComment(sanitize(inputCommentEl.value), sanitize(inputNameEl.value)).then((data) => {
+
+            document.querySelector('.form-loading').style.display = 'none'
+            document.querySelector('.add-form').style.display = 'flex'
+
             updateComments(data)
             renderComments()
             inputNameEl.value = ""
